3ed4fc97c837a3a4714c6157c70bb2ceb3885e51
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-11-13 11:07:00 +0100

    opdracht 1 af

--- opdracht1/script.js
+++ opdracht1/script.js
@@ -1,25 +1,29 @@
 // A) maak 2 verschillende variabelen met twee willekeurige integers (zelf kiezen)
 
 // jouw code hierondeer-->> 
-var ;
-var ;
+var getal = 40 ;
+var getal =70 ;
 
 // B) toon beide getallen op het scherm met document.getElementById('<jouw_gekozen_id>').innerHTML
 
 // jouw code hieronder-->>
-document.getElementById
+document.getElementById("main").innerHTML;
 
 // C) Vraag de gebruiker om een INPUT waarde door prompt() te gebruiken. 
 // Gebruik de prompt()-waarde om een variabelen te 'vullen'
 
 // jouw code hieronder-->>
-var ;
-var ;
+var getal1= prompt("welkom");
+var getal2= prompt("tot ziens") ;
 
 // D) Vergelijk de twee getallen met elkaar door gebruik te maken van conditions  if()
 
 // jouw code hieronder-->>
-if () {
+if (getal1 > getal2) 
+     function changeBackground(color) {
+        document.body.style.background = color;
+     }
+     
+     window.addEventListener("load",function() { changeBackground('red') });
 
-}
 
