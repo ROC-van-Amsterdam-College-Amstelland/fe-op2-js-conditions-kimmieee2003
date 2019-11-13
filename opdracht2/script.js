7296982bedef61ab401ca9bd5415bab2780a699e
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-11-13 11:48:08 +0100

    opdracht 2

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -5,7 +5,7 @@
  */
 
 /* jouw code hieronder-->> */
-var ingevuld_getal = ;
+var ingevuld_getal = document.getElementById("leeftijd").value ;
 
 /* B) Schrijf code waarmee je het ingevulde getal vergelijkt (condition) met de stemgerechtigde leeftijd van 18 jaar.
  * Als de leeftijd groter is dan 18 dan verander je de achtergrondkleur van de site met:
@@ -16,7 +16,16 @@ var ingevuld_getal = ;
 
 
 
+var leeftijd_die_mag_stemmen = 18;
+document.getElementById("main").style.backgroundColor = "purple";
+
 /* C) Schrijf code waarmee je het ingevulde getal vergelijkt (condition) met de stemgerechtigde leeftijd van 18 jaar.
  * Als de leeftijd kleiner is dan 18 dan verander je de achtergrondkleur van de site met:
                     document.getElementByTagName('body').style.backgroundColor = 'green'
  */
+function check(){
+    if( ingevuld_getal >= leeftijd_die_mag_stemmen  ){
+       document.getElementByTagName("main").style.backgroundColor = "green"
+    }
+
+}
