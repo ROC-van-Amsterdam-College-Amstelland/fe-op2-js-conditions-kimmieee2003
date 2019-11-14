dee54d7b8a50fbca52792cdd0eacc1e6e3ff0ddc
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2019-11-14 15:30:13 +0100

    commit hw2

--- hw-opdracht2/script.js
+++ hw-opdracht2/script.js
@@ -7,16 +7,48 @@ function zoekUit() {
 //als je met meer dan 2 personen kijkt en meer dan 3 uur per dag, dan is het beste abonnement familie
 //pseudocode: als (aantal==3 en duur>3 dan abonnement ="familie")
 
+if(aantal==3) {
+    document.getElementsByTagName ="familie"
+}
+
+if(duur>3) {
+    document.getElementsByTagName ="familie"
+}
+
+
 //als je met 2 of meer personen kijkt of al een abonnement hebt, dan is het beste abonnement uitgebreid
 //pseudocode: 
+
+if(aantal>=2) {
+    document.getElementsByTagName = "uitgebreid"
+}
 
 //als je met 2 personen kijkt of meer dan 4 uur per dag dan is het abonnement standaard
 //pseudocode: 
 
+if(aantal ==2) { 
+    document.getElementsByTagName = "standaard"
+}
+
+if(duur>4) {
+    document.getElementsByTagName = "standaard"
+}
+
+
 // als je met 1 persoon kijkt en 6 of meer uur per dag dan is het abonnement uitgebreid
 //pseudocode: 
+
+if(aantal==1) {
+    document.getElementsByTagName = "uitgebreid"
+}
+
+if(duur>6) {
+    document.getElementsByTagName = "uitgebreid"
+}
 
 
 //toon het type abonnement op het scherm
 
+function zoekUit(){
+    
 }
